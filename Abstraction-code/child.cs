54c5c3e3131d95126e2f33f30945ba0c1a78c0fd
pradeep <79890291+Pradeep-erode@users.noreply.github.com>
2021-05-06 20:24:44 +0530

    Add files via upload

---
+++ Abstraction-code/child.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    //inheritance of father class
+    public class Child:father
+    {
+        
+        //implementation of abstract class using override keyword whereas in method overriding this is not compulsary 
+        //to implement 
+
+
+        public override void display()
+        {
+            Console.WriteLine("I am implanted class");
+        }
+        public void disk()
+        {
+            Console.Write("I am pradeep");
+        }
+    }
+}
